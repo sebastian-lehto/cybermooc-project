305bda61f360d4e664eb03e258feecfa6cb166c9
Sebastian <sebastian.lehto@gmail.com>
2024-10-27 14:35:58 +0200

    individual profile pages

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class User(models.Model):
 	username = models.TextField()
+	uid = models.IntegerField()
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,10 +5,15 @@ from .models import User
 
 def index(request):
     User.objects.all().delete()
-    User.objects.create(username="Bob").save()
-    User.objects.create(username="Leah").save()
-    User.objects.create(username="Martin").save()
+    User.objects.create(username="Bob", uid=1).save()
+    User.objects.create(username="Leah", uid=2).save()
+    User.objects.create(username="Martin", uid=3).save()
 
     users = User.objects.all()
     context = {"users": users}
     return render(request, 'pages/index.html', context)
+
+def profileView(request, uid):
+    user = User.objects.get(uid=uid)
+    context = {"user": user}
+    return render(request, 'pages/profile.html', context)
